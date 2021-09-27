7e78a1d33cc1fbe7bff982875f851c9ffb120f7d
Gabriel Méndez <yo@gabrielmendezc.com>
2021-09-27 17:16:53 +0200

    feat(releases): improve semantic-release

--- release.config.js
+++ release.config.js
@@ -15,7 +15,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        assets: ['CHANGELOG.md'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
