6700b2a359be9fd767cb109a27c8eab8f383fc0f
Gabriel Méndez <yo@gabrielmendezc.com>
2021-09-27 17:34:54 +0200

    chore(commitlint): update body-max-line-length rule

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'type-enum': [2, 'always', Object.keys(types)],
     // Blow lines are optional.
     'header-max-length': [2, 'always', 100],
-    'body-max-line-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 250],
     'footer-max-line-length': [2, 'always', 100],
   },
 };
