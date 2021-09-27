7b3df4eb94f05b7730c557dfe53700e3c5cbabf1
Gabriel Méndez <yo@gabrielmendezc.com>
2021-09-27 16:51:00 +0200

    Husky test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Query, QuerySchema, QueryVariables } from './lib/query';
 export { Client, RegionGroupURL } from './lib/client';
-export { QueryError } from './lib/errors';
+export { QueryError} from './lib/errors';
 export { Expression, wrap, wrapValues } from './lib/expression';
