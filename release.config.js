88b010db5315d6731941ca13b8ccb15ffc700883
Gabriel Méndez <yo@gabrielmendezc.com>
2021-09-27 17:14:07 +0200

    chore(semantic-release): custom semantic-release config

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  branches: ['main'],
+  repositoryUrl: 'https://github.com/gmencz/faunadb.git',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+  preset: 'angular',
+};
